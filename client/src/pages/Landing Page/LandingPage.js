7a7e3d02dd11c9aac9ca674ebcd907db2afc25e0
Vinh <thomphuc13112003@gmail.com>
2026-05-24 23:14:02 +0700

    Remove landing page description text

--- client/src/pages/Landing Page/LandingPage.js
+++ client/src/pages/Landing Page/LandingPage.js
@@ -19,13 +19,6 @@ const LandingPage = () => {
     return (
       <main className="landingPage">
         <div className="landingPageContext">
-          <p className="landingPageText">
-            Chào mừng bạn đến với nhóm thuộc lớp Web Nâng Cao! Chúng tôi là các
-            chuyên gia về phát triển và tối ưu hóa website. Với kinh nghiệm lâu
-            năm và kiến thức chuyên sâu, chúng tôi cam kết mang đến cho bạn
-            những giải pháp tối ưu nhất để nâng cao sự hiệu quả cho trang web
-            của bạn.
-          </p>
           <button
             className="button"
             onClick={() => {
