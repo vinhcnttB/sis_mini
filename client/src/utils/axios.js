c3804b54d920c8b39cfd9ca033b1b68b5f150c5a
Vinh <thomphuc13112003@gmail.com>
2026-05-24 13:13:21 +0700

    Support production API URL env var

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 const getToken = () => JSON.parse(localStorage.getItem("token"));
 
-export const baseURL = "http://localhost:3334";
+export const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3334";
+
 export const axiosInstance = axios.create({
-    baseURL: baseURL,
-    timeout: 10000,
-    headers: {
-        Authorization: `Bearer ${getToken()}`,
-        post: {
-            "Content-Type": "application/json",
-        },
-        put: {
-            "Content-Type": "application/json",
-        },
+  baseURL: baseURL,
+  timeout: 10000,
+  headers: {
+    Authorization: `Bearer ${getToken()}`,
+    post: {
+      "Content-Type": "application/json",
     },
+    put: {
+      "Content-Type": "application/json",
+    },
+  },
 });
 
 // axios.interceptors.request.use(
@@ -41,35 +42,35 @@ export const axiosInstance = axios.create({
 // );
 
 const InterceptorsRequest = async (config) => {
-    // lấy token từ cookie và gắn vào header trước khi gửi request
-    const token = getToken();
-    if (token === undefined || token === null) {
-        return config;
-    }
-
-    const interceptorHeaders = {
-        token: `Bearer ${token}`,
-        authorization: `Bearer ${token}`,
-    };
-
-    const headers = {
-        ...config.headers,
-        ...interceptorHeaders,
-    };
-
-    config.headers = headers;
+  // lấy token từ cookie và gắn vào header trước khi gửi request
+  const token = getToken();
+  if (token === undefined || token === null) {
     return config;
+  }
+
+  const interceptorHeaders = {
+    token: `Bearer ${token}`,
+    authorization: `Bearer ${token}`,
+  };
+
+  const headers = {
+    ...config.headers,
+    ...interceptorHeaders,
+  };
+
+  config.headers = headers;
+  return config;
 };
 
 const InterceptorsError = (error) => {
-    return Promise.reject(error);
+  return Promise.reject(error);
 };
 
 const InterceptorResponse = (response) => {
-    if (response && response.data) {
-        return response;
-    }
+  if (response && response.data) {
     return response;
+  }
+  return response;
 };
 
 axiosInstance.interceptors.request.use(InterceptorsRequest, InterceptorsError);
